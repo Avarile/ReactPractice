ec5b2c039f4ae5eaabbf528f5f221bc6a73ff3af
Avarile <avarile@gmail.com>
2021-08-08 14:55:11 +1000

    commit on main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,51 @@
-import React from "react";
-import TestsPortal from "./Modules/DataManagement/TestsPortal";
+import { Input } from "@material-ui/icons";
+import React, { useState } from "react";
+
+const Todo = () => {
+  return (
+    <>
+      <p>Hello from todo</p>
+    </>
+  );
+};
+
+const Todos = () => {
+  return (
+    <>
+      <p>Hello from todos</p>
+    </>
+  );
+};
+
+const Inputs = () => {
+  const [todos, setState] = useState({
+    todos: [],
+  });
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.defaultPrevented;
+          setState();
+        }}
+      >
+        <label>Input your todos: </label>
+        <input
+          type="text"
+          name="todo"
+          onChange={(e) => setState(e.target.value)}
+        />
+      </form>
+    </>
+  );
+};
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <p>Hello from home</p>
+      <Inputs></Inputs>
+      <Todos></Todos>
+    </>
+  );
 }
