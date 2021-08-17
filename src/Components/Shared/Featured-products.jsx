dbddc3bf3c3c75908f2b9a291b421ce9b556a9fa
Avarile <avarile@gmail.com>
2021-08-17 10:31:27 +1000

    commit on main 'fix the problem in single product'

--- src/Components/Shared/Featured-products.jsx
+++ src/Components/Shared/Featured-products.jsx
@@ -1,27 +1,31 @@
-import React from "react"
+import React, { useContext } from "react"
 import { withRouter } from "react-router-dom"
 import "./Featured-products.styles.scss"
+// import { isInCart } from "../../Help"
+// import { CartContext } from "../../Context/Cart.context"
 
 function FeaturedProducts(props) {
-    const { title, imageUrl, price, history, id } = props // destruct the props it received
-    return (
-        <div className="featured-product">
-            <div
-                className="featured-image"
-                onClick={() => {
-                    history.push(`/product/${id}`)
-                }}>
-                <img src={imageUrl} alt="productImg" />
-            </div>
-            <div className="name-price">
-                <h3>{title}</h3>
-                <p>$ {[price]}</p>
-                <button className="button is-black nomad-btn">
-                    ADD TO CART
-                </button>
-            </div>
-        </div>
-    )
+  const { title, imageUrl, price, history, id, decription } = props // destruct the props it received
+  //   const { addProduct, cartItems } = useContext(CartContext)
+  const product = { title, imageUrl, price, id, decription }
+  return (
+    <div className="featured-product">
+      <div
+        className="featured-image"
+        onClick={() => {
+          history.push(`/product/${id}`)
+        }}>
+        <img src={imageUrl} alt="productImg" />
+      </div>
+      <div className="name-price">
+        <h3>{title}</h3>
+        <p>$ {price}</p>
+        <button className="button is-black nomad-btn" id="btn-white-outline">
+          ADD TO CART
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default withRouter(FeaturedProducts)
--- src/Components/SingleProduct/SingleProduct.jsx
+++ src/Components/SingleProduct/SingleProduct.jsx
@@ -4,57 +4,57 @@ import "./single-product.styles.scss"
 import { withRouter } from "react-router-dom"
 
 function SingleProduct({ match, history: { push } }) {
-    const { products } = useContext(ProductsContext)
-    const { id } = match.params
-    const [product, setProduct] = useState(null)
-    useEffect(() => {
-        const product = products.find(
-            (product) => Number(product.id) === Number(id)
-        )
-
-        // if product not exist, redirect to shop page
-        if (!product) {
-            return push("/shop")
-        }
-        setProduct(product)
-    }, [id, product, push, product, products])
-
-    if (!product) {
-        return null
-    }
-    // while check for product
-    const { imageUrl, title, price, description } = product
-
-    return (
-        <>
-            <div className="single-product-container">
-                <div className="product-image">
-                    <img src={imageUrl} alt="product-img" />
-                </div>
-                <div className="product-details">
-                    <div className="name-price">
-                        <h3>{title}</h3>
-                        <p>{price}</p>
-                    </div>
-                    <div className="btn-control">
-                        <button
-                            className="button is-white nomad-btn"
-                            id="btn-white-outline">
-                            Add Cart
-                        </button>
-                        <button
-                            className="button is-black nomad-btn"
-                            id="btn-white-outline">
-                            Proceed to CheckOut
-                        </button>
-                    </div>
-                    <div className="product-description">
-                        <p>{description}</p>
-                    </div>
-                </div>
-            </div>
-        </>
+  const { products } = useContext(ProductsContext)
+  const { id } = match.params
+  const [product, setProduct] = useState(null)
+  useEffect(() => {
+    const product = products.find(
+      (product) => Number(product.id) === Number(id)
     )
+
+    // if product not exist, redirect to shop page
+    if (!product) {
+      return push("/shop")
+    }
+    setProduct(product)
+  }, [id, product, push, product, products])
+
+  if (!product) {
+    return null
+  }
+  // while check for product
+  const { imageUrl, title, price, description } = product
+
+  return (
+    <>
+      <div className="single-product-container">
+        <div className="product-image">
+          <img src={imageUrl} alt="product-img" />
+        </div>
+        <div className="product-details">
+          <div className="name-price">
+            <h3>{title}</h3>
+            <p>{price}</p>
+          </div>
+          <div className="btn-control">
+            <button
+              className="button is-white nomad-btn"
+              id="btn-white-outline">
+              Add Cart
+            </button>
+            <button
+              className="button is-black nomad-btn"
+              id="btn-white-outline">
+              Proceed to CheckOut
+            </button>
+          </div>
+          <div className="product-description">
+            <p>{description}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default withRouter(SingleProduct)
