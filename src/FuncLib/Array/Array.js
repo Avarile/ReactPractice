53221f84698d5b72e16d236643096a25566ed1bd
Avarile <Avarile@gmail.com>
2021-12-08 23:48:12 +1000

    feat Javascript libs

--- src/FuncLib/Array/Array.js
+++ src/FuncLib/Array/Array.js
@@ -1,20 +1,110 @@
 // Section 1: Unique elements
-const array1 = ["Avarile", "Emily", "Anastasia", "Aurelia", "Emily", "Aurelia"]
-let newArray = Array.from(new Set(array1))
+const UniqueEle = () => {
+  const array1 = ["Avarile", "Emily", "Anastasia", "Aurelia", "Emily", "Aurelia"]
+  let newArray = Array.from(new Set(array1))
+
+  return newArray
+}
 // another solution
-let newArray = [...new Set(array1)]
-console.log(newArray)
-console.log(array1)
+const UniqueEle2 = (array) => {
+  let newArray = [...new Set(array)]
+  return newArray
+}
 
 // section 2: use spread operator to combine 2 arrays
-const array1 = ["Avarile", "Emily"]
-const array2 = ["Anastasia", "Aurelia"]
-const newArray = [...array1, ...array2]
-console.log(newArray)
+const CombineArrays = () => {
+  const array1 = ["Avarile", "Emily"]
+  const array2 = ["Anastasia", "Aurelia"]
+  const newArray = [...array1, ...array2]
+  console.log(newArray)
+}
 
 // section 3:
-const array1 = ["Avarile", "Emily", "Anastasia", "Aurelia", "Emily", "Aurelia"]
-const newArray = [...array1]
+const CopyArray = () => {
+  const array1 = ["Avarile", "Emily", "Anastasia", "Aurelia", "Emily", "Aurelia"]
+  const newArray = [...array1]
+  console.log(newArray)
+}
+// section 4: replace a certain element in the array
+const SpliceArray = () => {
+  const array1 = ["Avarile", "Emily", "Anastasia", "Aurelia"]
+  let result = array1.splice(2, 1, "grand father") // here we replaced the Anastasia with Grande father
 
-// section 4:
-let familyMembers = ["family"]
+  return console.log(result, array1)
+
+  // IMPORTANT!!! this func will change original array
+}
+
+// section 5: reset am array to enmpty
+const ResetArray = () => {
+  const array1 = ["Avarile", "Emily", "Anastasia", "Aurelia", "Emily", "Aurelia"]
+  array1.length = 0 // when an array.lentgh = 0, its emptied
+
+  return console.log(array1)
+}
+
+// section 6: turn an array in to object
+const ConvertIntoObj = () => {
+  const array1 = ["Avarile", "Emily", "Anastasia", "Aurelia", "Emily", "Aurelia"]
+  const arrayObj = { ...array1 }
+
+  return console.log(arrayObj)
+}
+
+// Fill some data in the array
+const FillArray = () => {
+  let array1 = new Array(8).fill("Avarile")
+
+  return console.log(array1)
+}
+
+// Find the common elements between arrays
+const FindCommonEle = () => {
+  const array1 = ["Avarile", "Emily", "Anastasia", "Aurelia", "Emily", "Aurelia"]
+  const array2 = ["Avarile", "Emily", "Anastasia", "grandFather", "grandeMother"]
+
+  const uniqueArray1 = [...new Set(array1)] // Before find the common element first get rid of the duplicates
+  const commonEle = uniqueArray1.filter((element) => array2.includes(element))
+  return console.log(commonEle)
+}
+
+// section 7: Get rid of the falsy values
+const DeleteFalsy = () => {
+  const array1 = ["Avarile", "Emily", null, undefined, false, "", NaN, "Anastasia", "Aurelia", "Emily", "Aurelia"]
+
+  const trueArray1 = array1.filter(Boolean)
+
+  return console.log(trueArray1)
+}
+
+// lastIndexOf() using this we can find the index of an element 's last presence
+const LastIndex = () => {
+  const array1 = ["Avarile", "Emily", "Anastasia", "Aurelia", "Emily", "Aurelia"]
+
+  const LastIndex = array1.lastIndexOf("Anastasia")
+  return console.log(LastIndex)
+}
+
+
+
+// From here Contents from the Red Book
+// Array.from()
+// Array.of()
+
+const ArrayFrom = () => {
+  const string = Array.from("Matt") // the string will be break in to 'm', 'a', 't', 't'
+  // from can also combine Sets and Map into an new array
+  const newMapArray = new Map().set("Avarile", 'Emily').set('Anastasia', 'Aurelia')
+  const newSetArray = new Set().add('Avarile').add("Emily").add('Anastasia').add('Aurelia')
+
+  // use From to shalow copy an array 
+  const array1 = [1, 2, 3, 4]
+  const array2 = new Array.from(array1)
+  // log(array1) [1, 2, 3, 4]
+  // alert( array1 === array2 )  false
+
+
+  const getArge
+}
+
+
