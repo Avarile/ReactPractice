6e321e3850aae9bcdf6c85507aab2e26fcc55888
Avarile <avarile@gmail.com>
2022-01-03 22:49:05 +1000

    feat data finished

--- src/JB_Hi_Fi/Data/ApiProvider.jsx
+++ src/JB_Hi_Fi/Data/ApiProvider.jsx
@@ -7,16 +7,48 @@ const ApiDataContext = React.createContext(undefined)
 ApiDataContext.displayName = "ApiDataContext"
 
 export const ApiDataProvider = ({ children }) => {
-  const [shipData, setShipData] = useState({})
+  const [playerData, setPlayerData] = useState({})
+  const [targetPlayerData, setTargetPlayerData] = useState({})
+  const [shipData, setShipData] = useState([])
+  const [weaponData, setWeaponData] = useState([])
+  const [spaceCraftData, setSpaceCraftData] = useState([])
   const TestData = {
     Mock: "I am Fucking Mock Data",
+  }
+
+  const usePlayerData = (playerName) => {
+    useEffect(() => {
+      axios
+        .request({
+          method: "get",
+          url: `http://localhost:4000/PLAYERS?name=${playerName}`,
+        })
+        .then((response) => {
+          setPlayerData(response.data)
+        })
+        .catch((error) => {
+          throw new Error(error)
+        })
+    }, [])
   }
 
   useEffect(() => {
     axios
       .request({
         method: "get",
-        url: "http://localhost:4000/players?name=Avarile",
+        url: "http://localhost:4000/PLAYERS?name=Avarile",
+      })
+      .then((response) => {
+        setPlayerData(response.data)
+      })
+      .catch((error) => {
+        throw new Error(error)
+      })
+
+    axios
+      .request({
+        method: "get",
+        url: "http://localhost:4000/SHIPS",
       })
       .then((response) => {
         setShipData(response.data)
@@ -24,9 +56,38 @@ export const ApiDataProvider = ({ children }) => {
       .catch((error) => {
         throw new Error(error)
       })
+
+    axios
+      .request({
+        method: "get",
+        url: "http://localhost:4000/WEAPONS",
+      })
+      .then((response) => {
+        setWeaponData(response.data)
+      })
+      .catch((error) => {
+        throw new Error(error)
+      })
+
+    axios
+      .request({
+        method: "get",
+        url: "http://localhost:4000/SPACECRAFTS",
+      })
+      .then((response) => {
+        setSpaceCraftData(response.data)
+      })
+      .catch((error) => {
+        throw new Error(error)
+      })
   }, [])
 
-  return <ApiDataContext.Provider children={children} value={{ TestData, shipData, setShipData }} />
+  return (
+    <ApiDataContext.Provider
+      children={children}
+      value={{ playerData, setPlayerData, shipData, setShipData, weaponData, setWeaponData, spaceCraftData, setSpaceCraftData, targetPlayerData, setTargetPlayerData }}
+    />
+  )
 }
 
 export const useApiData = () => {
