8e5149d204dbb38a0d1b962129b662dc24cb44c7
Avarile <avarile@gmail.com>
2022-01-08 22:55:41 +1000

    todo infoView 4 actions

--- src/JB_Hi_Fi/Data/Pics.js
+++ src/JB_Hi_Fi/Data/Pics.js
@@ -6,6 +6,14 @@ import Scout_Alpha from "../Assets/bgs/Troopers/Scout-Alpha.jpg"
 import Scout_Prima from "../Assets/bgs/Troopers/Scout-Prima.jpg"
 import Operator_Alpha from "../Assets/bgs/Troopers/Operator-Alpha.jpg"
 import Operator_Beta from "../Assets/bgs/Troopers/Operator-Beta.jpg"
+import Operator_Prima from "../Assets/bgs/Troopers/Operator-Prima.jpg"
+import Assualt_Prima from "../Assets/bgs/Troopers/Assualt-Prima.jpg"
+import Assualt_Alpha from "../Assets/bgs/Troopers/Assault-Alpha.jpg"
+import Attack_Alpha from "../Assets/bgs/Troopers/Attack-Alpha.jpg"
+import Attack_Prima from "../Assets/bgs/Troopers/Attack-Prima.jpg"
+import Defence_Alpha from "../Assets/bgs/Troopers/Defence-Alpha.jpg"
+import Defence_Prima from "../Assets/bgs/Troopers/Defence-Prima.jpg"
+import Commander from "../Assets/bgs/Troopers/Commander.jpg"
 
 export const shipPics = {
   Brisbane: BrisbaneClass,
@@ -18,8 +26,16 @@ export const spaceCraftPics = {
 }
 
 export const trooperPics = {
-  Scout_Alpha: Scout_Alpha,
-  Scout_Prima: Scout_Prima,
-  Operator_Alpha: Operator_Alpha,
-  Operator_Beta: Operator_Beta,
+  1: Scout_Alpha,
+  2: Scout_Prima,
+  3: Assualt_Prima,
+  4: Assualt_Alpha,
+  5: Attack_Alpha,
+  6: Attack_Prima,
+  7: Defence_Alpha,
+  8: Defence_Prima,
+  9: Commander,
+  10: Operator_Alpha,
+  11: Operator_Beta,
+  12: Operator_Prima,
 }
