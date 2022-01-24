baac2c4a8b63308d23064aebf4ea6db5962d882b
Avarile <Avarile@gmail.com>
2022-01-25 00:34:10 +1000

    trail videoplayer tomo

--- src/JB_Hi_Fi/MainIndex.jsx
+++ src/JB_Hi_Fi/MainIndex.jsx
@@ -6,7 +6,6 @@ import MainContent from "./Components/MainContent"
 import { MainDataProviders } from "./Data/MainDataEntrance"
 import Footer from "./Components/Footer"
 import { NotLoginScreen } from "./Views/NotLoginScreen"
-import { VideoPlayer } from "./Utls/VideoPlayer"
 
 import "./Styles/Main.css"
 
@@ -16,7 +15,6 @@ function MainIndex() {
   return (
     <MainDataProviders>
       <Router>
-        <VideoPlayer selector={"Jump"} />
         <SideBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <NavBar className="navbar" sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <NotLoginScreen />
